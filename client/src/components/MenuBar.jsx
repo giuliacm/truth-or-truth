d13a4a80b73d430a79b845a8a9377fc73ecf9aa2
Giulia Mattia <giuliamattia1@gmail.com>
2021-02-16 20:58:07 -0800

    Finished menu bar

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  menu: {
+    '&:focus': {
+      outline: 'none',
+    },
+  },
+}));
 
 const MenuBar = ({ username }) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
   const handleLogout = async (e) => {
     e.preventDefault();
     axios({
@@ -20,11 +44,54 @@ const MenuBar = ({ username }) => {
         console.log(err.response);
       });
   };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div>
+    <div className={classes.root}>
       <AppBar position="sticky">
         <Toolbar>
-          <Button onClick={handleLogout}>Logout</Button>
+          <Typography variant="h6" className={classes.title}>
+            Truth or Truth
+          </Typography>
+          <div>
+            <Button
+              size="large"
+              color="inherit"
+              className={classes.menu}
+              endIcon={<AccountCircle />}
+              onClick={handleMenu}
+            >
+              {username}
+            </Button>
+            <Menu
+              id="menu-appbar"
+              getContentAnchorEl={null}
+              anchorEl={anchorEl}
+              keepMounted
+              open={open}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+            >
+              <MenuItem component={Link} to="/games">
+                Games
+              </MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
--- client/src/theme.jsx
+++ client/src/theme.jsx
@@ -1,4 +1,5 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+// import { createMuiTheme } from '@material-ui/core/styles';
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
 
 const theme = createMuiTheme({
   palette: {
